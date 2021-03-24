4a627abeb2e3ab5c57229eeeb7e6cf811e3a9773
Paweł Piontkowski <piontkowski35@gmail.com>
2021-03-24 17:58:43 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
 /* Here goes your JS code */
+const btn = document.querySelector(".btn");
+
+const p = document.querySelector(".main p");
+
+const popup = document.querySelector("div.popup");
+
+const X = document.querySelector("div.X");
+
+const input = document.getElementById("button");
+
+btn.addEventListener("click",()=>{
+popup.style.display = "block";
+})
+
+X.addEventListener("click",()=>{
+    popup.style.display = "none";
+})
+
+input.addEventListener("click", ()=>{
+    popup.style.display = "none";
+    p.style.display= "block";
+    btn.style.display = "none";
+})
